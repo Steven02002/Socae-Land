1b650df3411c003d353202f5e4b16908d31669af
Steven02002 <170910215+Steven02002@users.noreply.github.com>
2025-09-18 17:05:36 -0400

    gulp, pruebaa extra

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 
-// Imagenes (DESACTIVADAS)
-// const cache = require('gulp-cache');
-// const imagemin = require('gulp-imagemin');
-// const webp = require('gulp-webp');
-// const avif = require('gulp-avif');
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 // Javascript
 const terser = require('gulp-terser-js');
@@ -32,6 +32,7 @@ function css() {
     return src(paths.scss)
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('public/build/css'));
 }
@@ -50,54 +51,49 @@ function javascript() {
           mode: 'production',
           entry: './src/js/app.js'
       }))
-      .pipe(sourcemaps.init())
       .pipe(terser())
-      .pipe(sourcemaps.write('.'))
       .pipe(rename({ suffix: '.min' }))
       .pipe(dest('./public/build/js'));
 }
 
-// --- TAREAS DE IMAGEN DESACTIVADAS ---
-// function imagenes() {
-//     return src(paths.imagenes)
-//         .pipe(cache(imagemin({ optimizationLevel: 3})))
-//         .pipe(dest('public/build/img'));
-// }
+// --- TAREAS DE IMAGEN ---
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('public/build/img'));
+}
 
-// function versionWebp(done) {
-//     const opciones = {
-//         quality: 50
-//     };
-//     src('src/img/**/*.{png,jpg}')
-//         .pipe(webp(opciones))
-//         .pipe(dest('public/build/img'));
-//     done();
-// }
+function versionWebp(done) {
+    const opciones = { quality: 50 };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('public/build/img'));
+    done();
+}
 
-// function versionAvif(done) {
-//     const opciones = {
-//         quality: 50
-//     };
-//     src('src/img/**/*.{png,jpg}')
-//         .pipe(avif(opciones))
-//         .pipe(dest('public/build/img'));
-//     done();
-// }
-
+function versionAvif(done) {
+    const opciones = { quality: 50 };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('public/build/img'));
+    done();
+}
 
 function dev(done) {
     watch(paths.scss, css);
     watch(paths.js, javascript);
-    // watch(paths.imagenes, imagenes); // Desactivado
-    // watch(paths.imagenes, versionWebp); // Desactivado
-    // watch(paths.imagenes, versionAvif); // Desactivado
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
+    watch(paths.imagenes, versionAvif);
     done();
 }
 
 exports.css = css;
 exports.js = javascript;
-// exports.imagenes = imagenes; // Desactivado
-// exports.versionWebp = versionWebp; // Desactivado
-// exports.versionAvif = versionAvif; // Desactivado
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 exports.dev = dev;
+
+// Para producción en Railway usa solo esto:
 exports.build = parallel(css, javascript);
